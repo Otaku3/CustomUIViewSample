e5fec447cc7f308aabae76d015203d8e8eed9772
otakumi <otakumi@m.cs.osakafu-u.ac.jp>
2018-11-13 13:26:35 +0900

    Initial Commit

---
+++ CustomUIViewSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomUIViewSample
+//
+//  Created by 大林拓実 on 2018/11/13.
+//  Copyright © 2018年 TakumiObayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
